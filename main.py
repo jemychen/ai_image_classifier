0e3ab7eee395e38977ded2ca2a7a19986201a973
Jemy Chen <jemychen@live.com>
2025-08-05 14:33:18 -0400

    Added load and process functions

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ from tensorflow.keras.applications.mobilenet_v2 import (
     decode_predictions
 )
 from PIL import Image
+
+def load_model():
+    model = MobileNetV2(weights ="imagenet")
+    return model
+
+def preprocess_image(image):
+    img = np.array(image)
+    img = cv2.resize(img, (224, 224))
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+    return img
+
